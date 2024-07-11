4f2371f93dde9147ed08c64f7d5beeb7f61f6a2a
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-07-11 02:46:00 +0000

    chg: add fallback to english

--- hiddifypanel_bot/utils/translation.py
+++ hiddifypanel_bot/utils/translation.py
@@ -1,8 +1,16 @@
+import i18n
 def setup_translation():
-    import i18n
     import hiddifypanel_bot
     i18n.load_path.append(hiddifypanel_bot.__path__[0]+'/translations')
     i18n.set('file_format', 'json')    
     i18n.set('skip_locale_root_data',True)
     i18n.set('filename_format', '{locale}.{format}')
+    i18n.set('fallback','en')
+    i18n.set('on_missing_translation',__on_missing__)
     i18n.set('placeholder_delimiter','$')
+
+def __on_missing__(key,locale,**kwargs):
+    if locale!='en':
+        return i18n.t(key,'en',**kwargs)    
+    return key
+
