ff93856ac1db29c34eabb5723595276e0ebc4080
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-07-11 19:38:21 +0000

    new: edit user

--- hiddifypanel_bot/modules/admin/__init__.py
+++ hiddifypanel_bot/modules/admin/__init__.py
@@ -1,1 +1,1 @@
-from . import welcome,add_user
+from . import welcome,add_user,search_user,edit_user
---
+++ hiddifypanel_bot/modules/admin/edit_user.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from hiddifypanel_bot import *
+from hiddifypanel_bot.utils import tghelper
+import telebot
+from telebot import types
+from i18n import t as _
+
+
+from . import constants as C
+
+
+def filter_for_via_bot(msg: HMessage):
+    if not msg.via_bot:
+        return
+    if not msg.reply_markup:
+        return
+    if len(msg.reply_markup.keyboard) != 1:
+        return
+    key = msg.reply_markup.keyboard[0][0]
+    if not key.callback_data:
+        return
+    if not key.callback_data.startswith("user_"):
+        return
+    return True
+
+
+@bot.message_handler(func=filter_for_via_bot)
+def user_resp_from_inline_query(msg: HMessage):
+    key = msg.reply_markup.keyboard[0][0]
+    uuid = key.callback_data.split("_")[-1]
+    bot.delete_message(msg.chat_id, msg.message_id)
+    user = msg.hapi.get_user(uuid)
+    send_user_info(msg, user)
+
+
+def send_user_info(msg, user: User):
+    resp = tghelper.format_user_message_from_admin(msg.lang, user)
+    keyboards = types.InlineKeyboardMarkup()
+    keyboards.add(
+        types.InlineKeyboardButton(text=_("admin.open_admin"), web_app=types.WebAppInfo(f"{msg.db['admin_link']}/admin/user/edit/?id={user['id']}")),
+        types.InlineKeyboardButton(text=_("admin.search_user"), switch_inline_query_current_chat="search "),
+    )
+    enable_key = types.InlineKeyboardButton(text=_("admin.user.enable"), callback_data=f"{C.USER_ENABLE}:{user['uuid']}")
+    if user["enable"]:
+        enable_key = types.InlineKeyboardButton(text=_("admin.user.disable"), callback_data=f"{C.USER_DISABLE}:{user['uuid']}")
+    keyboards.add(enable_key, types.InlineKeyboardButton(text=_("admin.user.delete"), callback_data=f"{C.USER_DELETE}:{user['uuid']}"))
+    keyboards.add(
+        types.InlineKeyboardButton(text=_("admin.user.reset_days"), callback_data=f"{C.USER_RESET_DAYS}:{user['uuid']}"),
+        types.InlineKeyboardButton(text=_("admin.user.reset_usage"), callback_data=f"{C.USER_RESET_USAGE}:{user['uuid']}"),
+    )
+
+    bot.send_message(msg.chat_id, resp, reply_markup=keyboards)
+
+
+@bot.callback_query_handler(call_action=C.USER_ENABLE, role=Role.AGENT)
+def enable_user_handler(call: HCallbackQuery):
+    uuid = call.data.split(":")[-1]
+    user = call.message.hapi.update_user(uuid, {"enable": True})
+    send_user_info(call.message, user)
+    bot.delete_message(call.message.chat_id, call.message.id)
+    bot.answer_callback_query(call.id)
+
+
+@bot.callback_query_handler(call_action=C.USER_ENABLE, role=Role.AGENT)
+def enable_user_handler(call: HCallbackQuery):
+    uuid = call.data.split(":")[-1]
+    user = call.message.hapi.update_user(uuid, {"enable": True})
+    send_user_info(call.message, user)
+    bot.delete_message(call.message.chat_id, call.message.id)
+    bot.answer_callback_query(call.id)
