68d9ad7cfc4463a72387e5dde4e1edeebd2d077b
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-07-11 19:37:57 +0000

    new: add user

---
+++ hiddifypanel_bot/modules/admin/constants.py
@@ -0,0 +1,8 @@
+ADD_USER = "admin.add_user"
+SERVER_INFO = "admin.server_info"
+ADMIN_USER = "admin.admin_users"
+USER_ENABLE = "admin.user.enable"
+USER_DISABLE = "admin.user.disable"
+USER_DELETE = "admin.user.delete"
+USER_RESET_DAYS = "admin.user.reset_days"
+USER_RESET_USAGE = "admin.user.reset_usage"
