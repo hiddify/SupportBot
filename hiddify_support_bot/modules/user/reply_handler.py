ba9dd209dda6c89e19a8d939cd6d10a4e54382e0
hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-07-28 02:23:39 +0200

    fix: bug

--- hiddify_support_bot/modules/user/reply_handler.py
+++ hiddify_support_bot/modules/user/reply_handler.py
@@ -60,7 +60,7 @@ async def reply_to_us(msg: HMessage):
 
 
 async def is_reply_to_user_condition_ignore_slash(msg: HMessage):
-    return is_reply_to_user_condition(msg, ignore_slash=True)
+    return await is_reply_to_user_condition(msg, ignore_slash=True)
 
 
 async def is_reply_to_user_condition(msg: HMessage, ignore_slash=False):
